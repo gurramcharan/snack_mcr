816d4819ab55f0e5458639c43e7c864ffaba99b4
Siva Charan <gurramsivacharan@gmail.com>
2023-06-23 23:05:14 +0530

    refractor: toggle added for sort

--- snack_time/src/pages/HomePage.jsx
+++ snack_time/src/pages/HomePage.jsx
@@ -1,38 +1,46 @@
-import React, { useContext } from 'react'
+import React, {useContext} from 'react'
 import {headings} from '../data/DataHeadings'
-import { DataContext } from '../context/DataContext'
+import {DataContext} from '../context/DataContext'
 import "./HomePage.css"
 
 export const HomePage = () => {
-    const {dataHeading,setDataHeading,filteredSnacks, setSearchData} = useContext(DataContext);
+    const {dataHeading, setDataHeading, filteredSnacks, setSearchData} = useContext(DataContext);
     const handleBtnClick = (e) => {
-        if (dataHeading === e) {
-            setDataHeading(`${e} asc`)
-            // setDataHeading(dataHeading => dataHeading = "")
-        } else {
-            setDataHeading(`${e} desc`)
-        }
+        setDataHeading(dataHeading.includes("asc")
+            ? `${e} desc`
+            : `${e} asc`)
     }
     return (
         <div className='main-container'>
-            <input className='search' type="text" name="search" id="search" placeholder='Search with Products or Ingredients...' onChange={(e) => setSearchData(e.target.value)} />
+            <input
+                className='search'
+                type="text"
+                name="search"
+                id="search"
+                placeholder='Search with Products or Ingredients...'
+                onChange={(e) => setSearchData(e.target.value)}/>
             <table className='table-container'>
                 <tr className='table-fields'>
                     {headings.map((head) => (
-                        <th className='table-fields'><button className='heading-btn' onClick={() => handleBtnClick(head)}>{head}</button></th>
+                        <th className='table-fields'>
+                            <button className='heading-btn' onClick={() => handleBtnClick(head)}>{head}</button>
+                        </th>
                     ))}
                 </tr>
                 {filteredSnacks.map((item) => (
-                    <tr  className='table-fields'>
-                        <td  className='table-fields'>{item.id}</td>
-                        <td  className='table-fields'>{item.product_name}</td>
-                        <td  className='table-fields'>{item.product_weight}</td>
-                        <td  className='table-fields'>{item.price}</td>
-                        <td  className='table-fields'>{item.calories}</td>
+                    <tr className='table-fields'>
+                        <td className='table-fields'>{item.id}</td>
+                        <td className='table-fields'>{item.product_name}</td>
+                        <td className='table-fields'>{item.product_weight}</td>
+                        <td className='table-fields'>{item.price}</td>
+                        <td className='table-fields'>{item.calories}</td>
                         <td>
-                        {item.ingredients.map((ing) => (
-                            <span> {ing},</span>
-                        ))}
+                            {item
+                                .ingredients
+                                .map((ing) => (
+                                    <span>
+                                        {ing},</span>
+                                ))}
                         </td>
                     </tr>
                 ))}
